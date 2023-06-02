1e97a7de97aca4aa1e100da1efe31a82903f53ab
Anton Glazunov <kampel2012@yandex.ru>
2023-06-02 18:08:07 +0500

    styles: корректировки по z индексам

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -18,12 +18,12 @@ const Sort = (props) => {
   };
 
   return (
-    <div className="text-sm font-bold self-center flex-wrap flex">
+    <div className="text-sm font-bold justify-end self-center flex-wrap flex">
       ▲ Сортировка по:{' '}
       <div className="relative group">
         <div
           onClick={togglePopupsActive}
-          className="hover:cursor-pointer ml-1 mb-2 w-24"
+          className="hover:cursor-pointer ml-1 md:mb-2 w-24"
         >
           <span className="text-orange-600 border-dashed font-normal border-b border-b-orange-600 ">
             {categoriesForSort[sort]}
@@ -31,7 +31,7 @@ const Sort = (props) => {
         </div>
 
         {isActivePopup && (
-          <ul className="overflow-hidden absolute cursor-pointer right-0 top-7 py-2 shadow rounded-lg font-normal text-sm w-32 z-10">
+          <ul className="bg-white overflow-hidden absolute cursor-pointer right-0 top-7 py-2 shadow rounded-lg font-normal text-sm w-32 z-10">
             {categoriesForSort.map((item, iter) => (
               <li
                 key={item}
--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -6,16 +6,20 @@ import Category from '../Categories';
 
 const Main = (props) => {
   return (
-    <div className="min-h-screen pb-1 container mx-auto rounded-3xl bg-white px-6">
-      <Header />
-      <div className="container mx-auto ">
-        <div className="flex flex-wrap justify-between">
+    <div className="min-h-screen pb-1 container mx-auto rounded-3xl bg-white">
+      <div className="mx-6">
+        <Header />
+      </div>
+      <div className="container mx-auto px-2 md:px-6">
+        <div className="flex justify-between">
           <Category />
           <Sort />
         </div>
 
-        <h2 className="font-bold text-3xl"> Все пиццы </h2>
-        <Gallery />
+        <h2 className="font-bold text-3xl ml-4"> Все пиццы </h2>
+        <div className="px-4 md:px-6">
+          <Gallery />
+        </div>
       </div>
     </div>
   );
