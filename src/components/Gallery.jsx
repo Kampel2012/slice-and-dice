4d07feffaee8995aca47e8700e607a5132ae4f51
Anton Glazunov <kampel2012@yandex.ru>
2023-06-02 13:31:06 +0500

    feature: перенес хранилище пицц и добавил axios

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,14 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductCard from './ProductCard';
-import { getPizzas } from './storage/pizzas.js';
+import axios from 'axios';
 
 const Gallery = (props) => {
   const [pizzaInfo, setPizzaInfo] = useState([]);
 
-  (async () => {
-    const res = await getPizzas();
-    setPizzaInfo(res);
-  })();
+  useEffect(() => {
+    async function getInitialPizzas() {
+      try {
+        const pizzas = await axios.get(
+          'https://6479a2f0a455e257fa6376cd.mockapi.io/items'
+        );
+        setPizzaInfo(pizzas.data);
+      } catch (error) {
+        console.warn(error);
+      }
+    }
+    getInitialPizzas();
+  }, []);
 
   return (
     <div className="mt-9 grid grid-cols gap-x-9 gap-y-16 xl:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 mb-20">
--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,31 +1,44 @@
 import React, { useState } from 'react';
 
 const Sort = (props) => {
-  const [sort, setSort] = useState('популярности');
+  const categoriesForSort = ['популярности', 'цене', 'алфавиту'];
+
+  const [sort, setSort] = useState(0);
   const [isActivePopup, setIsActivePopup] = useState(false);
 
-  const categoriesForSort = ['популярности', 'цене', 'алфавиту'];
+  const stylesCurrentSortItem = (item) =>
+    item === categoriesForSort[sort] &&
+    'font-bold text-orange-600 bg-orange-600 bg-opacity-5';
+
+  const togglePopupsActive = () => setIsActivePopup(!isActivePopup);
+
+  const onSelectItem = (item) => {
+    setSort(item);
+    togglePopupsActive();
+  };
 
   return (
     <div className="text-sm font-bold self-center flex-wrap hidden lg:flex">
       ▲ Сортировка по:{' '}
       <div className="relative group">
         <div
-          onClick={() => setIsActivePopup(!isActivePopup)}
+          onClick={togglePopupsActive}
           className="hover:cursor-pointer ml-1 mb-2 w-24"
         >
           <span className="text-orange-600 border-dashed font-normal border-b border-b-orange-600 ">
-            {sort}
+            {categoriesForSort[sort]}
           </span>
         </div>
 
         {isActivePopup && (
           <ul className="overflow-hidden absolute cursor-pointer right-0 top-7 py-2 shadow rounded-lg font-normal text-sm w-32">
-            {categoriesForSort.map((item) => (
+            {categoriesForSort.map((item, iter) => (
               <li
                 key={item}
-                onClick={() => setSort(item)}
-                className="hover:bg-orange-600 hover:bg-opacity-5 hover:font-bold hover:text-orange-600 px-2 py-2"
+                onClick={() => onSelectItem(iter)}
+                className={`hover:bg-orange-600 hover:bg-opacity-5 px-2 py-2 ${stylesCurrentSortItem(
+                  item
+                )}`}
               >
                 {item}
               </li>
