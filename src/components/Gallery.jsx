57da364d86147689e01eca015bfce0140897f96a
Anton Glazunov <kampel2012@yandex.ru>
2023-06-02 14:42:27 +0500

    feature добавлен скелетон для пицц (загрука)

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,29 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import ProductCard from './ProductCard';
+import ProductCard from './ProductCard/ProductCard';
 import axios from 'axios';
+import SceletonProductCard from './ProductCard/SceletonProductCard';
 
 const Gallery = (props) => {
   const [pizzaInfo, setPizzaInfo] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function getInitialPizzas() {
+    setIsLoading(true);
+    try {
+      const pizzas = await axios.get(
+        'https://6479a2f0a455e257fa6376cd.mockapi.io/items'
+      );
+      setPizzaInfo(pizzas.data);
+    } catch (error) {
+      console.warn(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
 
   useEffect(() => {
-    async function getInitialPizzas() {
-      try {
-        const pizzas = await axios.get(
-          'https://6479a2f0a455e257fa6376cd.mockapi.io/items'
-        );
-        setPizzaInfo(pizzas.data);
-      } catch (error) {
-        console.warn(error);
-      }
-    }
     getInitialPizzas();
   }, []);
 
   return (
     <div className="mt-9 grid grid-cols gap-x-9 gap-y-16 xl:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 mb-20">
-      {pizzaInfo.map((piz) => (
-        <ProductCard key={piz.id} {...piz} />
-      ))}
+      {isLoading
+        ? [...new Array(8)].map((item, i) => <SceletonProductCard key={i} />)
+        : pizzaInfo.map((piz) => <ProductCard key={piz.id} {...piz} />)}
     </div>
   );
 };
---
+++ src/components/ProductCard/SceletonProductCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ContentLoader from 'react-content-loader';
+
+const SceletonProductCard = (props) => {
+  return (
+    <ContentLoader
+      speed={2}
+      width={250}
+      height={460}
+      viewBox="0 0 250 460"
+      backgroundColor="#f3f3f3"
+      foregroundColor="#ecebeb"
+      {...props}
+    >
+      <circle cx="120" cy="120" r="120" />
+      <rect x="0" y="317" rx="10" ry="10" width="240" height="88" />
+      <rect x="0" y="252" rx="10" ry="10" width="240" height="48" />
+      <rect x="4" y="420" rx="10" ry="10" width="70" height="32" />
+      <rect x="134" y="420" rx="20" ry="20" width="110" height="32" />
+    </ContentLoader>
+  );
+};
+
+export default SceletonProductCard;
