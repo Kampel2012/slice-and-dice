b2d2857374dd6e453d806d2d02456a83a7a3336c
Anton Glazunov <kampel2012@yandex.ru>
2023-09-27 15:34:44 +0500

    refactor memo

--- src/components/UI/Category.tsx
+++ src/components/UI/Category.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { FC, memo } from 'react';
 
 interface CategoryProps {
@@ -7,25 +8,25 @@ interface CategoryProps {
   id: number;
 }
 
-const Category: FC<CategoryProps> = ({
-  textBtn,
-  isActive,
-  changeCategory,
-  id,
-}) => {
-  function changeCategoryOnClick() {
-    changeCategory(id);
+const Category: FC<CategoryProps> = memo(
+  ({ textBtn, isActive, changeCategory, id }) => {
+    function changeCategoryOnClick() {
+      changeCategory(id);
+    }
+
+    const stylesBtn = isActive
+      ? 'px-5 py-3 bg-black font-bold text-white rounded-full duration-200'
+      : 'px-5 py-3 bg-[#F9F9F9] font-bold text-[#2C2C2C] rounded-full duration-200';
+
+    return (
+      <button
+        type="button"
+        className={stylesBtn}
+        onClick={changeCategoryOnClick}>
+        {textBtn}
+      </button>
+    );
   }
+);
 
-  const stylesBtn = isActive
-    ? 'px-5 py-3 bg-black font-bold text-white rounded-full duration-200'
-    : 'px-5 py-3 bg-[#F9F9F9] font-bold text-[#2C2C2C] rounded-full duration-200';
-
-  return (
-    <button type="button" className={stylesBtn} onClick={changeCategoryOnClick}>
-      {textBtn}
-    </button>
-  );
-};
-
-export default memo(Category);
+export default Category;
